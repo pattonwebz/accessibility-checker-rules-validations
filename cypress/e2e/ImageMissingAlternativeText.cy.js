4cb33d6620616ab90c6ef77fbbf21d12a388597d
pattonwebz <will@pattonwebz.com>
2024-05-13 15:32:37 +0100

    Check the other issues for ImageMissingAlternativeText

--- cypress/e2e/ImageMissingAlternativeText.cy.js
+++ cypress/e2e/ImageMissingAlternativeText.cy.js
@@ -12,6 +12,15 @@ describe('Checking for images with missing alt text', () => {
 				assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
 			});
 			cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
+
+			// check if there are otherIssues
+			if (issueData.otherIssues !== 'undefined' && issueData.otherIssues) {
+				Object.entries(issueData.otherIssues).forEach(([key, value]) => {
+					cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+						assert(text === value.count, key + ' should have ' + value.count + ' issues');
+					});
+				});
+			}
 		});
 	});
 });
