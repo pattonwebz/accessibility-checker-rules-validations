5ebe4e7d60812f5b3881325ca438d868e1c6bef1
pattonwebz <will@pattonwebz.com>
2024-05-13 15:31:33 +0100

    Add a command to do the issue checking, save writing same code in many places

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -83,3 +83,25 @@ Cypress.Commands.add('recheckAndShowDetails', (postID) => {
 	cy.wait(100);
 	cy.get('.edac-tab').contains('Details').click();
 });
+
+Cypress.Commands.add('checkIssues', (key) => {
+	cy.loginAndActivate();
+
+	cy.fixture('errors.json').then((data) => {
+		const issueData = data[key];
+		cy.recheckAndShowDetails(issueData.postId);
+		cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+			assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
+		});
+		cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
+
+		// check if there are otherIssues
+		if (issueData.otherIssues !== 'undefined' && issueData.otherIssues) {
+			Object.entries(issueData.otherIssues).forEach(([key, value]) => {
+				cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+					assert(text === value.count, key + ' should have ' + value.count + ' issues');
+				});
+			});
+		}
+	});
+});
