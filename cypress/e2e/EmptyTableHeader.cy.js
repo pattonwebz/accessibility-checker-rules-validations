252a5af821d802754c6fae5c1de672309c5521a4
pattonwebz <will@pattonwebz.com>
2024-05-13 15:34:27 +0100

    Simplify contents of each test spec by using the command

--- cypress/e2e/EmptyTableHeader.cy.js
+++ cypress/e2e/EmptyTableHeader.cy.js
@@ -3,24 +3,6 @@ describe('Checking for Empty Table Headers', () => {
 	it('has issues', () => {
 		let key = 'Empty Table Header';
 
-		cy.loginAndActivate();
-
-		cy.fixture('errors.json').then((data) => {
-			const issueData = data[key];
-			cy.recheckAndShowDetails(issueData.postId);
-			cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
-				assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
-			});
-			cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
-
-			// check if there are otherIssues
-			if (issueData.otherIssues !== 'undefined' && issueData.otherIssues) {
-				Object.entries(issueData.otherIssues).forEach(([key, value]) => {
-					cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
-						assert(text === value.count, key + ' should have ' + value.count + ' issues');
-					});
-				});
-			}
-		});
+		cy.checkIssues(key);
 	});
 });
--- cypress/e2e/MissingTableHeader.cy.js
+++ cypress/e2e/MissingTableHeader.cy.js
@@ -3,15 +3,6 @@ describe('Checking for Missing Table Headers', () => {
 	it('has issues', () => {
 		let key = 'Missing Table Header';
 
-		cy.loginAndActivate();
-
-		cy.fixture('errors.json').then((data) => {
-			const issueData = data[key];
-			cy.recheckAndShowDetails(issueData.postId);
-			cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
-				assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
-			});
-			cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
-		});
+		cy.checkIssues(key);
 	});
 });
