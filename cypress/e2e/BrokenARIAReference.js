ae5ae32adac0733d41e7292303135176629133ef
pattonwebz <will@pattonwebz.com>
2024-05-13 15:32:23 +0100

    Add 4 more rule tests

---
+++ cypress/e2e/BrokenARIAReference.js
@@ -0,0 +1,17 @@
+describe('Checking for Broken ARIA References', () => {
+
+	it('has issues', () => {
+		let key = 'Broken ARIA Reference';
+
+		cy.loginAndActivate();
+
+		cy.fixture('errors.json').then((data) => {
+			const issueData = data[key];
+			cy.recheckAndShowDetails(issueData.postId);
+			cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+				assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
+			});
+			cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
+		});
+	});
+});
---
+++ cypress/e2e/MissingFormLabel.cy.js
@@ -0,0 +1,26 @@
+describe('Checking for Missing Form Labels', () => {
+
+	it('has issues', () => {
+		let key = 'Missing Form Label';
+
+		cy.loginAndActivate();
+
+		cy.fixture('errors.json').then((data) => {
+			const issueData = data[key];
+			cy.recheckAndShowDetails(issueData.postId);
+			cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+				assert(text === issueData.count, key + ' should have ' + issueData.count + ' issues');
+			});
+			cy.get('.edac-details .edac-details-rule-title h3').contains(key).scrollIntoView();
+
+			// check if there are otherIssues
+			if (issueData.otherIssues !== 'undefined' && issueData.otherIssues) {
+				Object.entries(issueData.otherIssues).forEach(([key, value]) => {
+					cy.get('.edac-details .edac-details-rule-title h3').contains(key).find('.edac-details-rule-count').invoke('text').then((text) => {
+						assert(text === value.count, key + ' should have ' + value.count + ' issues');
+					});
+				});
+			}
+		});
+	});
+});
